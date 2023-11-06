1f3c88b346dd2dc7e2d526a3838c0bae02e435db
Sange Sherpa <sherpasange1215@gmail.com>
2023-11-06 13:37:17 +0545

    Initial Commit

---
+++ swiftuitutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftuitutorial
+//
+//  Created by Sange Sherpa on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftuitutorial/swiftuitutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitutorialApp.swift
+//  swiftuitutorial
+//
+//  Created by Sange Sherpa on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
